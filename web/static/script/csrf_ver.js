13c3859331187e054cc219f3e5031f13bb03bdde
azzar <azzar.mr.zs@gmail.com>
2024-09-07 06:46:08 +0700

    nano

--- web/static/script/csrf_ver.js
+++ web/static/script/csrf_ver.js
@@ -4,7 +4,7 @@ fetch('/share_prompt', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'X-CSRFToken': csrfToken  // Include CSRF token
+        'X-CSRFToken': csrfToken
     },
     body: JSON.stringify(data)
 })
